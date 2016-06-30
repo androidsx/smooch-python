ad90e0c5d348faa1c59b52f785c2d39c6188a54d
Vladimir Melnichenko <melnichevv@gmail.com>
2016-06-30 12:03:59 +0300

    Code refactoring. Added docstings. Some improvements in appuser methods

--- smooch/__init__.py
+++ smooch/__init__.py
@@ -4,14 +4,17 @@ from __future__ import absolute_import, unicode_literals
 import logging
 import jwt
 import json
-from mimetypes import MimeTypes
 import os
 import requests
+
+from mimetypes import MimeTypes
 
 log = logging.getLogger(__name__)
 
 
 class Smooch:
+    ALGORITHM = 'HS256'
+
     class APIError(Exception):
         def __init__(self, response):
             self.response = response
@@ -22,15 +25,15 @@ class Smooch:
     def __init__(self, key_id, secret):
         self.key_id = key_id
         self.secret = secret
-        self.jwt_token = jwt.encode({'scope': 'app'}, secret, algorithm='HS256', headers={"kid": key_id})
+        self.jwt_token = jwt.encode({'scope': 'app'}, secret, algorithm=self.ALGORITHM, headers={"kid": key_id})
 
     @staticmethod
     def jwt_for_user(key_id, secret, user_id):
-        return jwt.encode({'scope': 'appUser', 'userId': user_id}, secret, algorithm='HS256', headers={"kid": key_id})
+        return jwt.encode({'scope': 'appUser', 'userId': user_id}, secret, algorithm=Smooch.ALGORITHM,
+                          headers={"kid": key_id})
 
     def user_jwt(self, user_id):
         return self.jwt_for_user(self.key_id, self.secret, user_id)
-
 
     def ask(self, endpoint, data, method='get', files=None):
         url = "https://api.smooch.io/v1/{0}".format(endpoint)
@@ -58,11 +61,10 @@ class Smooch:
 
         response = caller_func(url=url, headers=headers, data=data, files=files)
 
-        if response.status_code == 200 or response.status_code == 201:
+        if response.status_code in [200, 201]:
             return response
         else:
             raise Smooch.APIError(response)
-
 
     def post_message(self, user_id, message, sent_by_maker=False):
         role = "appUser"
@@ -71,7 +73,6 @@ class Smooch:
 
         data = {"text": message, "role": role}
         return self.ask('appusers/{0}/conversation/messages'.format(user_id), data, 'post')
-
 
     def post_media(self, user_id, file_path, sent_by_maker=False):
         role = "appUser"
@@ -90,42 +91,102 @@ class Smooch:
 
         return self.ask(url, data, 'post', files)
 
-
     def get_user(self, user_id):
+        """
+        Retrieve a specific app user.
+        Args:
+            user_id (str): A unique identifier for the app user.
+        """
         return self.ask('appusers/{0}'.format(user_id), {}, 'get')
 
-    def update_user(self, user_id, data):
+    def update_user(self, user_id, data=None):
+        """
+        Update an app user’s basic profile information
+        Args:
+            user_id (int): A unique identifier for the app user.
+            data (dict, optional): Can contain `givenName`, `surname`, `email`, `signUpAt`, `properties` values
+        """
+        if data is None:
+            data = {}
         return self.ask('appusers/{0}'.format(user_id), data, 'put')
 
-    def init_user(self, user_id, device_id):
+    def init_user(self, device_id, user_id=None):
+        """
+        This API is called when the app is first loaded.
+        Args:
+            device (str): A descriptor of the user’s device.
+            user_id (str, optional): A unique identifier for the app user.
+        """
         data = {
             "device": {
                 "id": device_id,
                 "platform": "other"
-            },
-            "userId": user_id
+            }
         }
+        if user_id:
+            data.update({'userId': user_id})
         return self.ask('init', data, 'post')
 
-    def precreate_user(self, user_id):
-        data = {
-            "userId": user_id
-        }
+    def precreate_user(self, user_id, data=None):
+        """
+        Pre-Create App User
+        Args:
+            user_id (int): A unique identifier for the app user.
+            data (dict, optional): Can contain `givenName`, `surname`, `email`, `signUpAt`, `properties` values
+        """
+        if data is None:
+            data = {}
+
+        data.update({"userId": user_id})
+
         return self.ask('appusers', data, 'post')
 
     def get_webhooks(self):
+        """
+        List webhooks
+        """
         return self.ask('webhooks', {}, 'get')
 
-    def make_webhook(self, target, triggers):
+    def make_webhook(self, target, triggers=None):
+        """
+        Create webhook
+        Args:
+            target (str): URL to be called when the webhook is triggered.
+            triggers (array, optional): An array of triggers you wish to have the webhook listen to. Default trigger is message.
+        """
+        if triggers is None:
+            triggers = ['message']
+
         return self.ask('webhooks', {"target": target, "triggers": triggers}, 'post')
 
+    def get_webhook(self, webhook_id):
+        """
+        Get webhook
+        """
+        return self.ask('webhooks/{0}'.format(webhook_id), {}, 'get')
+
     def update_webhook(self, webhook_id, target, triggers):
+        """
+        Update webhook
+        Args:
+            webhook_id (str): webhook ID.
+            target (str): URL to be called when the webhook is triggered.
+            triggers (array, optional): An array of triggers you wish to have the webhook listen to. Default trigger is message.
+        """
         return self.ask('webhooks/{0}'.format(webhook_id), {"target": target, "triggers": triggers}, 'put')
 
     def delete_webhook(self, webhook_id):
+        """
+        Deletes the specified webhook.
+        Args:
+            webhook_id (str): webhook ID
+        """
         return self.ask('webhooks/{0}'.format(webhook_id), {}, 'delete')
 
     def delete_all_webhooks(self):
+        """
+        Deletes all webhook using get_webhooks method.
+        """
         webhooks_response = self.get_webhooks()
         webhooks = webhooks_response.json()['webhooks']
 
@@ -136,8 +197,7 @@ class Smooch:
 
         return responses
 
-
-    def ensure_webhook_exist(self, trigger, webhook_url):
+    def ensure_webhook_exists(self, trigger, webhook_url):
         log.debug("Ensuring that webhook exist: %s; %s", trigger, webhook_url)
         r = self.get_webhooks()
         data = r.json()
@@ -175,5 +235,3 @@ class Smooch:
             'Authorization': 'Bearer {0}'.format(self.jwt_token),
             'content-type': 'application/json'
         }
-
-
